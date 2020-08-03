9d4c038b17e01269ea715e60825a56a2b82a8b9c
Alexander Barthel <albar965@mailbox.org>
2020-08-03 20:47:30 +0200

    3.5.2.develop

--- src/atools.cpp
+++ src/atools.cpp
@@ -34,7 +34,7 @@ const static QChar SEP(QDir::separator());
 
 QString version()
 {
-  return "3.5.1.develop"; // VERSION_NUMBER - atools
+  return "3.5.2.develop"; // VERSION_NUMBER - atools
 }
 
 QString gitRevision()
