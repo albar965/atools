4253c19668d2f011e5930191efafde6f84a25303
Alexander Barthel <albar965@mailbox.org>
2020-11-18 12:09:08 +0100

    3.6.5.rc1

--- src/atools.cpp
+++ src/atools.cpp
@@ -34,7 +34,7 @@ const static QChar SEP(QDir::separator());
 
 QString version()
 {
-  return "3.6.4.beta"; // VERSION_NUMBER - atools
+  return "3.6.5.rc1"; // VERSION_NUMBER - atools
 }
 
 QString gitRevision()
