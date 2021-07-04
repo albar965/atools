52e7c01e69be0ffd722fe2419ec6e6d4d34c222c
Alexander Barthel <albar965@mailbox.org>
2021-07-04 23:22:08 +0200

    Version 3.6.14

--- src/atools.cpp
+++ src/atools.cpp
@@ -34,7 +34,7 @@ const static QChar SEP(QDir::separator());
 
 QString version()
 {
-  return "3.6.13"; // VERSION_NUMBER - atools
+  return "3.6.14"; // VERSION_NUMBER - atools
 }
 
 QString gitRevision()
