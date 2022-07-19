507ea9597bca6452587e7451aeab62c852cf8d2c
Alexander Barthel <albar965@mailbox.org>
2022-07-19 13:27:46 +0200

    Fixed database connection leak when compiling X-Plane scenery library.
    # Conflicts:
    #	src/fs/xp/xpwriter.h

--- src/fs/xp/xpdatacompiler.cpp
+++ src/fs/xp/xpdatacompiler.cpp
@@ -613,6 +613,12 @@ void XpDataCompiler::close()
   delete airportWriter;
   airportWriter = nullptr;
 
+  delete airportMsaWriter;
+  airportMsaWriter = nullptr;
+
+  delete holdingWriter;
+  holdingWriter = nullptr;
+
   delete airwayPostProcess;
   airwayPostProcess = nullptr;
 
--- src/fs/xp/xpwriter.h
+++ src/fs/xp/xpwriter.h
@@ -68,18 +68,28 @@ protected:
       return line.at(index);
     else
       // Have to stop reading the file since the rest can be corrupted
-      throw atools::Exception(ctx->messagePrefix() +
-                              QString(": Index out of bounds: Index: %1, size: %2").arg(index).arg(line.size()));
+      throw atools::Exception(ctx->messagePrefix() + QString(": Index out of bounds: Index: %1, size: %2").arg(index).arg(line.size()));
   }
 
+  /* Returns empty string for airport ident ENRT (enroute) */
+  QString atAirportIdent(const QStringList& line, int index)
+  {
+    if(index < line.size())
+    {
+      const QString& str = line.at(index).simplified();
+      return str == "ENRT" ? QString() : str;
+    }
+    else
+      // Have to stop reading the file since the rest can be corrupted
+      throw atools::Exception(ctx->messagePrefix() + QString(": Index out of bounds: Index: %1, size: %2").arg(index).arg(line.size()));
+  }
   QString mid(const QStringList& line, int index, bool ignoreError = false)
   {
     if(index < line.size())
       return line.mid(index).join(" ");
     else if(!ignoreError)
       // Have to stop reading the file since the rest can be corrupted
-      throw atools::Exception(ctx->messagePrefix() +
-                              QString(": Index out of bounds: Index: %1, size: %2").arg(index).arg(line.size()));
+      throw atools::Exception(ctx->messagePrefix() + QString(": Index out of bounds: Index: %1, size: %2").arg(index).arg(line.size()));
     return QString();
   }
 
