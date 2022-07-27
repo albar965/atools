9d436fa1a2425085e4db1832b53dd9384d9166df
Alexander Barthel <albar965@mailbox.org>
2022-07-27 14:29:59 +0200

    Added sample radius function to GlobeReader and increased sample size for lines for more accuracy.

--- src/fs/common/globereader.h
+++ src/fs/common/globereader.h
@@ -52,15 +52,17 @@ public:
   bool openFiles();
   void closeFiles();
 
-  /* Elevation in meter */
-  float getElevation(const atools::geo::Pos& pos);
+  /* Elevation in meter. If "distanceMeter" is > 0 then the center and four points around will be checked too.
+   * "sampleRadiusMeter" defines a rectangle where five points are sampled and the maximum is used.*/
+  float getElevation(const atools::geo::Pos& pos, float sampleRadiusMeter = 0.f);
 
   /* Get elevations along a great circle line. Will create a point every 500 meters and delete
-   * consecutive ones with same elevation */
-  void getElevations(geo::LineString& elevations, const atools::geo::LineString& linestring);
+   * consecutive ones with same elevation
+   * "sampleRadiusMeter" defines a rectangle where five points are sampled and the maximum is used.*/
+  void getElevations(geo::LineString& elevations, const atools::geo::LineString& linestring, float sampleRadiusMeter = 0.f);
 
 private:
-  friend class::DtmTest;
+  friend class ::DtmTest;
 
   /* Source data parameters */
   static Q_DECL_CONSTEXPR int NUM_DATAFILES = 16;
@@ -73,9 +75,10 @@ private:
   static Q_DECL_CONSTEXPR int GRID_ROWS = 2 * 6000 + 2 * 4800;
 
   /* Distance between sampling points meter */
-  static Q_DECL_CONSTEXPR float INTERPOLATION_SEGMENT_LENGTH = 250.f;
+  static Q_DECL_CONSTEXPR float INTERPOLATION_SEGMENT_LENGTH_M = 100.f;
+
   /* Points are considered equal if they are equal within this range in meter */
-  static Q_DECL_CONSTEXPR float SAME_ELEVATION_EPSILON = 1.f;
+  static Q_DECL_CONSTEXPR float SAME_ELEVATION_EPSILON_M = 1.f;
 
   /* Calculate file index and byte offset within file */
   qint64 calcFileOffset(int gridCol, int gridRow, int& fileIndex);
@@ -84,6 +87,8 @@ private:
   static bool fileEntryValid(const QFileInfo& fileEntry);
   void closeFile(int i);
   void openFile(int i);
+  float elevationFromIndexAndOffset(int fileIndex, qint64 fileOffset);
+  float elevationMax(const atools::geo::Pos& pos, float sampleRadiusMeter);
 
   QString dataDir;
   QVector<QString> dataFilenames;
