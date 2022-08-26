c581a0ff30f1d22e9337f6e000ffe3ad7937b84e
Alexander Barthel <albar965@mailbox.org>
2022-08-26 17:07:05 +0200

    Fixed compilation issues on older Linux.

--- src/fs/sc/datareaderthread.cpp
+++ src/fs/sc/datareaderthread.cpp
@@ -29,13 +29,18 @@
 #include <QCoreApplication>
 #include <QDir>
 
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+using Qt::hex;
+using Qt::dec;
+using Qt::endl;
+#endif
+
 namespace atools {
 namespace fs {
 namespace sc {
 
 void DataReaderThread::debugWriteWhazzup(const atools::fs::sc::SimConnectData& dataPacket)
 {
-  using Qt::endl;
   static QDateTime last;
 
   // VATSIM
