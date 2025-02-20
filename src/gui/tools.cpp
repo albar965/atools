4227cd240e6b7435c38b973c30a2524f067a9fc3
Alexander Barthel <albar965@mailbox.org>
2025-02-20 13:04:17 +0100

    Small correction for logging message box texts

--- src/gui/tools.cpp
+++ src/gui/tools.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
-* Copyright 2015-2024 Alexander Barthel alex@littlenavmap.org
+* Copyright 2015-2025 Alexander Barthel alex@littlenavmap.org
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -223,15 +223,15 @@ void logMessageBox(QWidget *parent, QMessageBox::Icon icon, const QString& text)
     case QMessageBox::NoIcon:
     case QMessageBox::Information:
     case QMessageBox::Question:
-      qInfo().noquote().nospace() << Q_FUNC_INFO << " parent " << parentName << plainText;
+      qInfo().noquote().nospace() << Q_FUNC_INFO << " parent " << parentName << " message \"" << plainText << "\"";
       break;
 
     case QMessageBox::Warning:
-      qWarning().noquote().nospace() << Q_FUNC_INFO << " parent " << parentName << plainText;
+      qWarning().noquote().nospace() << Q_FUNC_INFO << " parent " << parentName << " message \"" << plainText << "\"";
       break;
 
     case QMessageBox::Critical:
-      qCritical().noquote().nospace() << Q_FUNC_INFO << " parent " << parentName << plainText;
+      qCritical().noquote().nospace() << Q_FUNC_INFO << " parent " << parentName << " message \"" << plainText << "\"";
       break;
   }
 }
