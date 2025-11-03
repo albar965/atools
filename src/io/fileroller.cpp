7d0cd39d45ffd8d36339910d02ffa6f2f31dc125
Alexander Barthel <albar965@mailbox.org>
2025-11-03 14:15:40 +0100

    Fixed warning in fileroller

--- src/io/fileroller.cpp
+++ src/io/fileroller.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
-* Copyright 2015-2023 Alexander Barthel alex@littlenavmap.org
+* Copyright 2015-2025 Alexander Barthel alex@littlenavmap.org
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@ FileRoller::FileRoller(int maxNumFiles, const QString& filePattern, bool keepOri
 
 void FileRoller::rollFiles(const QStringList& filenames)
 {
-  for(QString fn : filenames)
+  for(const QString& fn : filenames)
     rollFile(fn);
 }
 
