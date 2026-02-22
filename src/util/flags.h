aa929218fa3e4d4a97ea847cfc460a891306e276
Alexander Barthel <albar965@mailbox.org>
2026-02-22 13:31:55 +0100

    Fix for static assert in Flags

--- src/util/flags.h
+++ src/util/flags.h
@@ -33,7 +33,7 @@ class Flags
 {
   /* Print error message at compile time if size of enum and template parameter FLAGTYPE differs */
   static_assert(sizeof(ENUM) == sizeof(FLAGTYPE), "atools::util::Flags: sizeof(ENUM) != sizeof(FLAGTYPE)");
-  static_assert(std::is_convertible<ENUM, long long>(), "std::is_convertible<ENUM, long long>()");
+  static_assert(std::is_convertible<FLAGTYPE, quint64>(), "std::is_convertible<FLAGTYPE, quint64>()");
 
 public:
   /* Create types for template parameters. */
